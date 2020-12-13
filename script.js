8d4c9f457a169f341e6535e670b44107278aa05c
T Schway <tony.schwebach@gmail.com>
2020-12-13 15:07:56 -0500

    added logic for correct or wrong notification

--- script.js
+++ script.js
@@ -7,18 +7,21 @@ let startButtonEl = document.getElementById("start-button");
 
 // Variables
 let currentQuestion = 0;
+
 // array of objects for questions{question: Text, choices:[1,2,3,4], answer:choices[i]}
 let jsQuizQuestions = [
   {
     question: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts",
+    answerIndex: 2,
   },
   {
     question:
       "The condition in an if / else statement is enclosed within ____.",
     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses",
+    answerIndex: 2,
   },
   {
     question: "Arrays in JavaScript can be used to store ____.",
@@ -29,18 +32,21 @@ let jsQuizQuestions = [
       "all of the above",
     ],
     answer: "all of the above",
+    answerIndex: 3,
   },
   {
     question:
       "String values must be enclosed within ____ when being assigned to variables.",
     choices: ["commas", "curly brackets", "quotes", "parentheses"],
     answer: "quotes",
+    answerIndex: 2,
   },
   {
     question:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["JavaScript", "terminal/bash", "for loops", "console.log()"],
     answer: "console.log()",
+    answerIndex: 3,
   },
 ];
 
@@ -48,6 +54,7 @@ let jsQuizQuestions = [
 function startQuiz() {
   questionTextEl.textContent = "";
   instructionsEl.textContent = "";
+  startButtonEl.setAttribute("class","d-none"); //do not display start button once game begins
   currentQuestion = 0; // new quiz reset to start question
   askQuestion(currentQuestion);
 
@@ -68,18 +75,24 @@ function askQuestion(questionNumber) {
     choicesListEl.appendChild(liEl);
     let buttonEl = document.createElement("button"); //create buttons within li element
     buttonEl.setAttribute("class", ""); //bootstrap classes here
-    buttonEl.id = "option" + j; //for event listener event delegation
+    buttonEl.setAttribute("data-choiceIndex", j); //for event listener event delegation
     buttonEl.textContent =
       j + 1 + ". " + jsQuizQuestions[questionNumber].choices[j];
     liEl.appendChild(buttonEl);
   }
 }
 
+// event listener for user selection of choices for the question
 choicesListEl.addEventListener("click", function (event) {
   event.preventDefault();
   if (event.target.matches("button")) {
-    var userSelection = event.target.id;
-    console.log(userSelection);
+    var userSelection = parseInt(event.target.getAttribute("data-choiceIndex"));
+    if (userSelection === jsQuizQuestions[currentQuestion].answerIndex){
+      notificationEl.textContent = "Correct!";
+    } else{
+      notificationEl.textContent = "Wrong!";
+    }
+
     currentQuestion++;
     if (currentQuestion < jsQuizQuestions.length) {
       askQuestion(currentQuestion);
@@ -89,6 +102,7 @@ choicesListEl.addEventListener("click", function (event) {
   }
 });
 
+// event listener to start the game
 startButtonEl.addEventListener("click", function (event) {
   event.preventDefault();
   startQuiz();
